da0f43c1b15eb32a295bff772038b84a5594e878
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2016-08-19 16:40:05 -0700

    Make it object orinted

--- two_body.cpp
+++ two_body.cpp
@@ -1,4 +1,5 @@
 // Two body representation
+// TODO: plug the huge number of potential memory leaks
 #include <iostream>
 #include <stdio.h>
 #include <cmath> //abs
@@ -46,24 +47,6 @@ void dump_vector(string const &title, vector<double> dat)
 {
     cout<<title;
     printf(" : [%f, %f, %f]\n", dat[0], dat[1], dat[2]);
-}
-
-vector<double> rv_coe (vector<double> r, vector<double> v)
-{
-    double k = 398600.4415;
-    vector<double> z = {0., 0., 1.0};
-    vector<double> h = cross_product(r, v);
-    double norm_h = norm(h);
-    vector<double> n = vec_scale(1.0/norm_h, cross_product(z, h));
-    vector<double> e = vec_scale((1.0/k), vec_add(vec_scale(dot_product(v, v)-(k/norm(r)), r), vec_scale(-1.0*dot_product(r, v),v)));
-    double ecc = norm(e);
-    double p = dot_product(h, h) / k;
-    double a = p / (1.0 - pow(ecc, 2.0));
-    double inc = acos(h[2]/norm(h));
-    double raan = atan2(n[1], n[0]);
-    double argp = atan2(dot_product(e, vec_scale(1.0/norm_h, cross_product(h, n))), dot_product(e, n));
-    double nu = atan2(dot_product(r, vec_scale(1.0/norm_h, cross_product(h, e))), dot_product(r, e));
-    return vector<double> { a, ecc, inc, raan, argp, nu};
 }
 
 double c2(double psi)
@@ -118,10 +101,53 @@ double c3(double psi)
     return res;
 }
 
-vector<vector<double>> propagate(vector<double> r0, vector<double> v0, float tof, int numiter, float rtol)
+class EarthOrbit
+{
+    int numiter = 35;
+    double rtol = 1e-10;
+    double k = 398600.4415;
+    void rv2coe();
+  public:
+    vector<double> r;
+    vector<double> v;
+    double ecc;
+    double p;
+    double a;
+    double inc;
+    double raan;
+    double argp;
+    double nu;
+    EarthOrbit(vector<double>, vector<double>);
+    void propagate(double);
+};
+
+EarthOrbit::EarthOrbit (vector<double> r_in, vector<double> v_in){
+    r = r_in;
+    v = v_in;
+    rv2coe();
+}
+
+void EarthOrbit::rv2coe()
+{
+    vector<double> z = {0., 0., 1.0};
+    vector<double> h = cross_product(r, v);
+    double norm_h = norm(h);
+    vector<double> n = vec_scale(1.0/norm_h, cross_product(z, h));
+    vector<double> e = vec_scale((1.0/k), vec_add(vec_scale(dot_product(v, v)-(k/norm(r)), r), vec_scale(-1.0*dot_product(r, v),v)));
+    ecc = norm(e);
+    p = dot_product(h, h) / k;
+    a = p / (1.0 - pow(ecc, 2.0));
+    inc = acos(h[2]/norm(h));
+    raan = atan2(n[1], n[0]);
+    argp = atan2(dot_product(e, vec_scale(1.0/norm_h, cross_product(h, n))), dot_product(e, n));
+    nu = atan2(dot_product(r, vec_scale(1.0/norm_h, cross_product(h, e))), dot_product(r, e));
+}
+
+void EarthOrbit::propagate(double tof)
 {
     //Compute Lagrange coefficients
-    double k = 398600.4415;
+    vector<double> r0 = r;
+    vector<double> v0 = v;
     double dot_r0v0 = dot_product(r0, v0);
     double norm_r0 = norm(r0);
     double sqrt_mu = pow(k, .5);
@@ -175,29 +201,23 @@ vector<vector<double>> propagate(vector<double> r0, vector<double> v0, float tof
     double g = tof - pow(xi, 3) / sqrt_mu * c3_psi;
     double gdot = 1.0 - pow(xi, 2) / norm_r * c2_psi;
     double fdot = sqrt_mu / (norm_r * norm_r0) * xi * (psi * c3_psi - 1.0);
-    printf("Tolerance: %f\n", f*gdot - fdot*g - 1.0);
-    vector<double> r = vec_add(vec_scale(f, r0), vec_scale(g, v0));
-    vector<double> v = vec_add(vec_scale(fdot, r0), vec_scale(gdot, v0));
-    return vector<vector<double>> {r, v};
+    r = vec_add(vec_scale(f, r0), vec_scale(g, v0));
+    v = vec_add(vec_scale(fdot, r0), vec_scale(gdot, v0));
 }
 
-vector<double> earth_orbit()
-{
-    vector<double> r = {-6045., -3490., 2500.};
-    vector<double> v = {-3.457, 6.618, 2.533};
-    vector<vector<double>> rv_new = propagate(r, v, 5.0, 35, 1e-10);
-    dump_vector("R", rv_new[0]);
-    dump_vector("V", rv_new[1]);
-    return rv_coe(r, v);
-}
 
 int main()
 {
-    vector<double>orbital_coe = earth_orbit();
-    printf ("Semi-major Axis: %f\n", orbital_coe[0]);
-    printf ("Eccentricity: %f\n", orbital_coe[1]);
-    printf ("Inclination: %f\n", orbital_coe[2]);
-    printf ("Right Ascension of Ascending Node: %f\n", orbital_coe[3]);
-    printf ("Argument of Pericenter: %f\n", orbital_coe[4]);
-    printf ("True anomoly: %f\n", orbital_coe[5]);
+    vector<double> r = {-6045., -3490., 2500.};
+    vector<double> v = {-3.457, 6.618, 2.533};
+    EarthOrbit ship_orbit (r, v);
+    printf ("Semi-major Axis: %f\n", ship_orbit.a);
+    printf ("Eccentricity: %f\n", ship_orbit.ecc);
+    printf ("Inclination: %f\n", ship_orbit.inc);
+    printf ("Right Ascension of Ascending Node: %f\n", ship_orbit.raan);
+    printf ("Argument of Pericenter: %f\n", ship_orbit.argp);
+    printf ("True anomoly: %f\n", ship_orbit.nu);
+    ship_orbit.propagate(5.0);
+    dump_vector("New R", ship_orbit.r);
+    dump_vector("New V", ship_orbit.v);
 }
