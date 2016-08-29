e49b5a66584ac527e29c31e0a9de64e8ea91015a
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2016-08-29 01:50:30 -0700

    Multiple Satellites

--- src/system.h
+++ src/system.h
@@ -90,15 +90,18 @@ void updateEarthPhase()
 }
 
 class gameSystem {
+    private:
+        int idx = -1;
+        glm::mat4 projection;
     public:
         vector<Shader> shaderBank;
         vector<Model> modelBank;
-        vector<Cube> cubeBank;
-        vector<Ellipse3d> eBank;
         vector<Satellite> sBank;
         gameSystem(GLuint, GLuint);
         void processKeys(GLfloat);
         void step();
+        void AddSatellite();
+        void RemoveSatellite();
 };
 
 void gameSystem::processKeys(GLfloat deltaTime)
@@ -113,21 +116,37 @@ void gameSystem::processKeys(GLfloat deltaTime)
     if(keys[GLFW_KEY_D])
         camera.ProcessKeyboard(RIGHT, deltaTime);
     if(keys[GLFW_KEY_UP])
-        sBank[0].thrustForward(timeFactor*timeResolution);
+        sBank[idx].thrustForward(timeFactor*timeResolution);
     if(keys[GLFW_KEY_DOWN])
-        sBank[0].thrustBackward(timeFactor*timeResolution);
+        sBank[idx].thrustBackward(timeFactor*timeResolution);
     if(keys[GLFW_KEY_LEFT])
-        sBank[0].thrustLeft(timeFactor*timeResolution);
+        sBank[idx].thrustLeft(timeFactor*timeResolution);
     if(keys[GLFW_KEY_RIGHT])
-        sBank[0].thrustRight(timeFactor*timeResolution);
+        sBank[idx].thrustRight(timeFactor*timeResolution);
     if(keys[GLFW_KEY_PAGE_UP])
-        sBank[0].thrustUp(timeFactor*timeResolution);
+        sBank[idx].thrustUp(timeFactor*timeResolution);
     if(keys[GLFW_KEY_PAGE_DOWN])
-        sBank[0].thrustDown(timeFactor*timeResolution);
+        sBank[idx].thrustDown(timeFactor*timeResolution);
     if(keys[GLFW_KEY_Q])
         timeFactor++;
     if(keys[GLFW_KEY_E] && timeFactor>0)
         timeFactor--;
+    if(keys[GLFW_KEY_F])
+        this->AddSatellite();
+    if(keys[GLFW_KEY_R])
+        this->RemoveSatellite();
+
+}
+
+void gameSystem::AddSatellite(){
+    Satellite mySat(projection);
+    sBank.push_back(mySat);
+    idx++;
+}
+
+void gameSystem::RemoveSatellite(){
+    sBank.erase(sBank.begin()+idx);
+    idx--;
 }
 
 gameSystem::gameSystem(GLuint screenWidth, GLuint screenHeight){
@@ -143,7 +162,7 @@ gameSystem::gameSystem(GLuint screenWidth, GLuint screenHeight){
     //glBlendFunc (GL_ONE, GL_ONE);
     glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glm::mat4 projection = glm::perspective(45.0f, (GLfloat)screenWidth/(GLfloat)screenHeight, .1f, 100.0f);
+    projection = glm::perspective(45.0f, (GLfloat)screenWidth/(GLfloat)screenHeight, .1f, 100.0f);
 
     Shader planetShader("shaders/planet.vs", "shaders/planet.frag");
     shaderBank.push_back(planetShader);
@@ -151,10 +170,7 @@ gameSystem::gameSystem(GLuint screenWidth, GLuint screenHeight){
     modelBank.push_back(planet);
     this->shaderBank[0].Use();
     glUniformMatrix4fv(glGetUniformLocation(this->shaderBank[0].Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-
-    Satellite mySat(projection);
-    sBank.push_back(mySat);
-
+    this->AddSatellite();
 }
 
 void gameSystem::step(){
@@ -174,8 +190,9 @@ void gameSystem::step(){
     glUniformMatrix4fv(glGetUniformLocation(this->shaderBank[0].Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
     modelBank[0].Draw(this->shaderBank[0]);
     updateEarthPhase();
-
-    sBank[0].Render(view);
-    sBank[0].orbit_.propagate(timeFactor*timeResolution);
+    for (int i = 0; i<=idx; i++) {
+        sBank[i].Render(view);
+        sBank[i].orbit_.propagate(timeFactor*timeResolution);
+    }
 }
 #endif // GAME_SYSTEM_H_
